dd49d6a957c9590fb3dcbfb8d2d365dafb0f83fb
mzq777 <1033628976@qq.com>
2019-09-03 10:00:12 +0800

    DataTable与List转换
    DataTable与List转换

---
+++ DataTableListHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ts.Component
+{
+    public class DataTableListHelper
+    {
+        #region 单例
+        private static readonly Lazy<DataTableListHelper> instance = new Lazy<DataTableListHelper>(() => new DataTableListHelper());
+        private DataTableListHelper() { }
+        public static DataTableListHelper Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        #endregion
+        /// <summary>
+        /// Convert a List{T} to a DataTable.
+        /// </summary>
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in props)
+            {
+                Type t = GetCoreType(prop.PropertyType);
+                tb.Columns.Add(prop.Name, t);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[props.Length];
+                for (int i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+                tb.Rows.Add(values);
+            }
+            return tb;
+        }
+        /// <summary>
+        /// Determine of specified type is nullable
+        /// </summary>
+        public static bool IsNullable(Type t)
+        {
+            return !t.IsValueType || (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+        /// <summary>
+        /// Return underlying type if type is Nullable otherwise return the type
+        /// </summary>
+        public static Type GetCoreType(Type t)
+        {
+            if (t != null && IsNullable(t))
+            {
+                if (!t.IsValueType)
+                {
+                    return t;
+                }
+                else
+                {
+                    return Nullable.GetUnderlyingType(t);
+                }
+            }
+            else
+            {
+                return t;
+            }
+        }
+        /// <summary>
+        /// DataTable转成List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public List<T> DataTableToList<T>(DataTable dt)
+        {
+            var list = new List<T>();
+            var plist = new List<PropertyInfo>(typeof(T).GetProperties());
+            foreach (DataRow item in dt.Rows)
+            {
+                T s = Activator.CreateInstance<T>();
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    PropertyInfo info = plist.Find(p => p.Name.ToUpper() == dt.Columns[i].ColumnName.ToUpper());
+                    if (info != null)
+                    {
+                        try
+                        {
+                            if (!Convert.IsDBNull(item[i]))
+                            {
+                                object v = null;
+                                if (info.PropertyType.ToString().Contains("System.Nullable"))
+                                {
+                                    v = Convert.ChangeType(item[i], Nullable.GetUnderlyingType(info.PropertyType));
+                                }
+                                else
+                                {
+                                    v = Convert.ChangeType(item[i], info.PropertyType);
+                                }
+                                info.SetValue(s, v, null);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception("字段[" + info.Name + "]转换出错," + ex.Message);
+                        }
+                    }
+                }
+                list.Add(s);
+            }
+            return list;
+        }
+    }
+}
