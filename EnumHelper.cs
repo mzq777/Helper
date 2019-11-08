8e11dabe09be10db3c5c68e10411322e39b89ec2
mzq777 <35713668+mzq777@users.noreply.github.com>
2019-11-08 14:32:08 +0800

    Add files via upload
    枚举转换为List

---
+++ EnumHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace HostServiceForNeusoft.Utils
+{
+    public class EnumHelper
+    {
+        /// <summary>
+        /// 将枚举转为集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static List<EnumEntity> EnumToList<T>()
+        {
+            List<EnumEntity> list = new List<EnumEntity>();
+
+            foreach (var e in Enum.GetValues(typeof(T)))
+            {
+                EnumEntity m = new EnumEntity();
+                object[] objArr = e.GetType().GetField(e.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (objArr != null && objArr.Length > 0)
+                {
+                    DescriptionAttribute da = objArr[0] as DescriptionAttribute;
+                    m.Desction = da.Description;
+                }
+                m.EnumValue = Convert.ToInt32(e);
+                m.EnumName = e.ToString();
+                list.Add(m);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 获取枚举描述
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="EnumName"></param>
+        /// <returns></returns>
+        public static string GetEnumDesction<T>(string EnumName)
+        {
+            foreach (var e in Enum.GetValues(typeof(T)))
+            {
+                if (e.ToString() != EnumName)
+                    continue;
+
+                EnumEntity m = new EnumEntity();
+                object[] objArr = e.GetType().GetField(e.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (objArr != null && objArr.Length > 0)
+                {
+                    DescriptionAttribute da = objArr[0] as DescriptionAttribute;
+                    return da.Description;
+                }
+            }
+            return "";
+        }
+    }
+
+    /// <summary>
+    /// 枚举实体
+    /// </summary>
+    public class EnumEntity
+    {
+        /// <summary>  
+        /// 枚举的描述  
+        /// </summary>  
+        public string Desction { set; get; }
+
+        /// <summary>  
+        /// 枚举名称  
+        /// </summary>  
+        public string EnumName { set; get; }
+
+        /// <summary>  
+        /// 枚举对象的值  
+        /// </summary>  
+        public int EnumValue { set; get; }
+    }
+}
